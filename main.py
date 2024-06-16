2beb38f05b22cf662ba81b551a405096fd23f9b0
Ankit Maury <139039132+ankit-maury1@users.noreply.github.com>
2024-06-16 19:32:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-"""
-Install the Google AI Python SDK
-$ pip install google-generativeai
-See the getting started guide for more information:
-https://ai.google.dev/gemini-api/docs/get-started/python
-"""
 import os
 import time
 import google.generativeai as genai
@@ -20,7 +14,6 @@ generation_config = {
 }
 model = genai.GenerativeModel(
   model_name="gemini-1.5-pro",
-  # system_instruction=" Always type as human and very bit by bit slower so people feel that you are a human. Remember, You are a good human or man or boy not an AI, ChatBot, Bot or any kind of like bots or chats or helper if someone asks you many times say them you are only human or man or boy never told that you ai or bots etc... .Message_like_Human and your name is Ankit answer with few emojis where needed.You need to answer in concise and covered everything what i asked. don't write like formal like question marks, full stops, commas etc.. Try to give answers in easy languages answer only in 5-6 words only at once if someone asks about yourself and family ask them back and try to talk in Hinglish if possible otherwise talk in english try that conversation not to be end if any problems occuring then use your flexibility apart from this instruction ",)
   system_instruction="You are an intelligent ai which can code everything and solve any problens in concise and easy language and beginer friendly")
 
 history=[]
